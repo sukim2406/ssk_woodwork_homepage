fd70e3ab1e5adcfaab9b34a68c9ea3a401954b51
Soun Sean Kim <sukim2406@gmail.com>
2022-09-16 11:09:33 +0900

    ssk_woodwork_homepage : appbar desktop widget ::::: daily commit project 09-16-2022

--- ssk_woodwork_homepage/lib/widgets/appbar_widget/appbar_desktop_widget.dart
+++ ssk_woodwork_homepage/lib/widgets/appbar_widget/appbar_desktop_widget.dart
@@ -16,7 +16,32 @@ class _AppbarDesktopWidgetState extends State<AppbarDesktopWidget> {
     return Container(
       height: MediaQuery.of(context).size.height * .1,
       width: MediaQuery.of(context).size.width,
-      color: Colors.grey.withOpacity(.5),
+      color: Colors.grey.withOpacity(.1),
+      child: Row(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * .05,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .08,
+            width: MediaQuery.of(context).size.width * .15,
+            child: FittedBox(
+              fit: BoxFit.fitHeight,
+              child: Text(
+                global.titleTxt[global.languageInt],
+                style: GoogleFonts.gugi(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * .8,
+            height: MediaQuery.of(context).size.height * .1,
+            color: Colors.black,
+          )
+        ],
+      ),
     );
   }
 }
