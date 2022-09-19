3b7eb61f69522c21d52ac03c66e5de65f8b82e48
Soun Sean Kim <sukim2406@gmail.com>
2022-09-19 18:02:34 +0900

    ssk_woodwork_homepage : mobile app bar :::::: daily commit project 09-19-2022

--- ssk_woodwork_homepage/lib/pages/landing_page/landing_mobile_page.dart
+++ ssk_woodwork_homepage/lib/pages/landing_page/landing_mobile_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../widgets/appbar_widget/appbar_widget.dart';
 import '../../widgets/welcome_widget/welcome_widget.dart';
 import '../../widgets/appbar_widget/drawer_mobile_widget.dart';
+import '../../widgets/appbar_widget/appbar_mobile_widget.dart';
 
 class LandingMobilePage extends StatefulWidget {
   const LandingMobilePage({Key? key}) : super(key: key);
@@ -16,14 +17,14 @@ class _LandingMobilePageState extends State<LandingMobilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: DrawerMobileWidget(),
-      appBar: AppBar(),
+      appBar: AppbarMobileWidget(),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.white,
         child: Column(
           children: const [
-            AppbarWidget(), // height : .1 * h
+            // AppbarWidget(), // height : .1 * h
             WelcomeWidget(), // height : .8 * h
           ],
         ),
--- ssk_woodwork_homepage/lib/widgets/appbar_widget/appbar_mobile_widget.dart
+++ ssk_woodwork_homepage/lib/widgets/appbar_widget/appbar_mobile_widget.dart
@@ -3,71 +3,83 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../controllers/global_controller.dart' as global;
 
-class AppbarMobileWidget extends StatefulWidget {
-  const AppbarMobileWidget({Key? key}) : super(key: key);
+class AppbarMobileWidget extends StatelessWidget
+    implements PreferredSizeWidget {
+  const AppbarMobileWidget({super.key});
 
   @override
-  State<AppbarMobileWidget> createState() => _AppbarMobileWidgetState();
-}
+  Size get preferredSize => Size.fromHeight(60);
 
-class _AppbarMobileWidgetState extends State<AppbarMobileWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * .1,
-      width: MediaQuery.of(context).size.width,
-      color: Colors.white,
-      child: Row(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .1,
-          ),
-          Expanded(
-            child: Container(),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .5,
-            height: MediaQuery.of(context).size.height * .08,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .07,
-                  child: FittedBox(
-                    fit: BoxFit.fitHeight,
-                    child: Text(
-                      global.titleTxt[global.languageInt],
-                      style: GoogleFonts.gugi(),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .05,
-                  child: FittedBox(
-                    fit: BoxFit.fitHeight,
-                    child: Text(
-                      global.titleTxt2[global.languageInt],
-                      style: GoogleFonts.gugi(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .1,
-            height: MediaQuery.of(context).size.height * .08,
-            child: const Icon(
-              Icons.more_vert_outlined,
-            ),
-          ),
-        ],
-      ),
-    );
+    return AppBar();
   }
 }
+// class AppbarMobileWidget extends StatefulWidget {
+//   const AppbarMobileWidget({Key? key}) : super(key: key);
+
+//   @override
+//   State<AppbarMobileWidget> createState() => _AppbarMobileWidgetState();
+// }
+
+// class _AppbarMobileWidgetState extends State<AppbarMobileWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: MediaQuery.of(context).size.height * .1,
+//       width: MediaQuery.of(context).size.width,
+//       color: Colors.white,
+//       child: Row(
+//         children: [
+//           SizedBox(
+//             width: MediaQuery.of(context).size.width * .1,
+//           ),
+//           Expanded(
+//             child: Container(),
+//           ),
+//           SizedBox(
+//             width: MediaQuery.of(context).size.width * .5,
+//             height: MediaQuery.of(context).size.height * .08,
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 SizedBox(
+//                   height: MediaQuery.of(context).size.height * .07,
+//                   child: FittedBox(
+//                     fit: BoxFit.fitHeight,
+//                     child: Text(
+//                       global.titleTxt[global.languageInt],
+//                       style: GoogleFonts.gugi(),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   height: MediaQuery.of(context).size.height * .05,
+//                   child: FittedBox(
+//                     fit: BoxFit.fitHeight,
+//                     child: Text(
+//                       global.titleTxt2[global.languageInt],
+//                       style: GoogleFonts.gugi(
+//                         color: Colors.grey,
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           Expanded(
+//             child: Container(),
+//           ),
+//           SizedBox(
+//             width: MediaQuery.of(context).size.width * .1,
+//             height: MediaQuery.of(context).size.height * .08,
+//             child: const Icon(
+//               Icons.more_vert_outlined,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
