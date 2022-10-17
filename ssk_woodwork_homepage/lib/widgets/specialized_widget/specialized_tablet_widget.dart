63a20fa8a80380fade9ce421e30137196fb87036
Soun Sean Kim <sukim2406@gmail.com>
2022-10-17 22:38:01 +0900

    ssk_woodwork_homepage : specialized widget tablet ::::: daily commit project 10-17-2022

--- ssk_woodwork_homepage/lib/widgets/specialized_widget/specialized_tablet_widget.dart
+++ ssk_woodwork_homepage/lib/widgets/specialized_widget/specialized_tablet_widget.dart
@@ -15,6 +15,24 @@ class _SpecializedTabletWidgetState extends State<SpecializedTabletWidget> {
       height: MediaQuery.of(context).size.height * .775,
       width: MediaQuery.of(context).size.width,
       color: Colors.purple,
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * .4,
+            width: MediaQuery.of(context).size.width * .8,
+            child: Image.asset(
+              'img/temp/temp_img_3.png',
+              fit: BoxFit.contain,
+            ),
+            color: Colors.pink,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .375,
+            width: MediaQuery.of(context).size.width * .8,
+            child: Text('맞춤 재작'),
+          )
+        ],
+      ),
     );
   }
 }
