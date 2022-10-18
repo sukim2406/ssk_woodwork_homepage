377c3368f0b488c196efb2d89e912f9ad603ab5c
Soun Sean Kim <sukim2406@gmail.com>
2022-10-18 22:36:49 +0900

    ssk_woodwork_homepage : desktop specialized widget ::::: daily commit project 10-18-2022

--- ssk_woodwork_homepage/lib/widgets/specialized_widget/specialized_desktop_widget.dart
+++ ssk_woodwork_homepage/lib/widgets/specialized_widget/specialized_desktop_widget.dart
@@ -15,6 +15,23 @@ class _SpecializedDesktopWidgetState extends State<SpecializedDesktopWidget> {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * .85,
       color: Colors.purple,
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * .55,
+            width: MediaQuery.of(context).size.width * .8,
+            child: Image.asset(
+              'img/temp/temp_img_3.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            width: MediaQuery.of(context).size.width * .8,
+            child: Text('맞춤 재작'),
+          ),
+        ],
+      ),
     );
   }
 }
