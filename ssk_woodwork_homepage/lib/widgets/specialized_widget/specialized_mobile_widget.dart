5698996889e9c03ad395ec9bb4231fd353656d06
Soun Sean Kim <sukim2406@gmail.com>
2022-10-21 22:58:08 +0900

    ssk_woodwork_homeapge : mobile specialized widget ::::: daily commit project 10-21-2022

--- ssk_woodwork_homepage/lib/widgets/specialized_widget/specialized_mobile_widget.dart
+++ ssk_woodwork_homepage/lib/widgets/specialized_widget/specialized_mobile_widget.dart
@@ -17,9 +17,12 @@ class _SpecializedMobileWidgetState extends State<SpecializedMobileWidget> {
       color: Colors.purple,
       child: Column(
         children: [
+          Expanded(
+            child: Container(),
+          ),
           Container(
             width: MediaQuery.of(context).size.width * .8,
-            height: MediaQuery.of(context).size.height * .4,
+            height: MediaQuery.of(context).size.height * .5,
             color: Colors.red,
             child: Image.asset(
               'img/temp/temp_img_3.png',
@@ -27,10 +30,40 @@ class _SpecializedMobileWidgetState extends State<SpecializedMobileWidget> {
             ),
           ),
           SizedBox(
-            height: MediaQuery.of(context).size.height * .4,
+            height: MediaQuery.of(context).size.height * .15,
             width: MediaQuery.of(context).size.width * .8,
-            child: Text('맞춤 재작'),
-          )
+            child: Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * .075,
+                  width: MediaQuery.of(context).size.width * .8,
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: Text('맞춤 제작'),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * .0375,
+                  width: MediaQuery.of(context).size.width * .8,
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: Text('문의 주세요'),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * .0375,
+                  width: MediaQuery.of(context).size.width * .8,
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: Text('000-000-0000'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
         ],
       ),
     );
