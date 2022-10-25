f490945adc6607cf9db521a29935486eb6610f6b
Soun Sean Kim <sukim2406@gmail.com>
2022-10-25 22:46:51 +0900

    ssk_woodwork_homepage : originals widget mobile ::::: daily commit project 10-25-2022

---
+++ ssk_woodwork_homepage/lib/widgets/originals_widget/originals_mobile_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OriginalsMobileWidget extends StatefulWidget {
+  const OriginalsMobileWidget({super.key});
+
+  @override
+  State<OriginalsMobileWidget> createState() => _OriginalsMobileWidgetState();
+}
+
+class _OriginalsMobileWidgetState extends State<OriginalsMobileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
