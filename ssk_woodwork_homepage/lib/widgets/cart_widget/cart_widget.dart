92b97789dbe053081752a3869bb826140d01fbe8
Soun Sean Kim <sukim2406@gmail.com>
2022-11-14 20:52:57 +0900

    ssk_woodwork_homepage : cart widget ::::: daily commit project 11-14-2022

---
+++ ssk_woodwork_homepage/lib/widgets/cart_widget/cart_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../responsive_layout_widget.dart';
+
+import '../cart_widget/cart_mobile_widget.dart';
+
+class CartWidget extends StatefulWidget {
+  const CartWidget({super.key});
+
+  @override
+  State<CartWidget> createState() => _CartWidgetState();
+}
+
+class _CartWidgetState extends State<CartWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayoutWidget(
+      mobile: CartMobileWidget(),
+    );
+  }
+}
