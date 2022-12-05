a8713816d5267be43afa9517d0f8088e6758a60d
Soun Sean Kim <sukim2406@gmail.com>
2022-12-05 17:43:38 +0900

    ssk_woodwork_homeapge : mobile funiture widget ::::: daily commit proejct 12-05-2022

---
+++ ssk_woodwork_homepage/lib/widgets/funiture_widget/funiture_mobile_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FunitureMobileWidget extends StatefulWidget {
+  const FunitureMobileWidget({super.key});
+
+  @override
+  State<FunitureMobileWidget> createState() => _FunitureMobileWidgetState();
+}
+
+class _FunitureMobileWidgetState extends State<FunitureMobileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
